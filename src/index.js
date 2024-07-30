34f5bea3283a0b41e4a3046caf3db275151576a7
EduardDanilov <ed.danilov94@gmail.com>
2024-07-30 22:40:06 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const inputNumber = n.toString();
+    if (inputNumber[0] === '-') {
+        const result = inputNumber.slice(1).split('').reverse().join('');
+        return +result;
+    } else {
+        return +inputNumber.split('').reverse().join('');
+    }
 }
